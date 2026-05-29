63f408f2a0227c2054435a2a064192501418e44c
Md Irfan <mdirfancse2023@gmail.com>
2026-05-29 12:05:28 +0530

    Configure Azure deployment workflow

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/mdirfanportfolio" element={<Index />} />
+          <Route path="/mdirfanportfolio/*" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
