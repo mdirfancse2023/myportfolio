41a093ccdc65eed742259a9f8cc1e8eb8e442c75
Md Irfan <mdirfancse2023@gmail.com>
2026-06-06 13:38:10 +0530

    fixed path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/myportfolio/" : "/",
+  base: mode === "production" ? "/My_Portfolio/" : "/",
   server: {
     host: "::",
     port: 8080,
